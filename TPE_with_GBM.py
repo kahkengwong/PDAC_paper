d4f6084e2bb0eaea552143439ffaca69a18f1990
Kah Keng Wong <48384560+kahkengwong@users.noreply.github.com>
2024-04-17 08:33:10 +0800

    Update TPE_with_GBM.py

--- TPE_with_GBM.py
+++ TPE_with_GBM.py
@@ -67,17 +67,17 @@ def plot_tpe_performance_seaborn(trials, num_genes, csf_name, max_evals):
 def run_analysis(max_evals, n_startup_jobs, gene_list, csf, csf_name):
     np.random.seed(1) 
     random.seed(1) 
-    full_data = pd.read_excel("PDAC_Ctrl_All_Cases_Split-60-Training_n=82.xlsx")
+    train_data = pd.read_excel("PDAC_Ctrl_All_Cases_Split-60-Training_n=82.xlsx")
     test_data = pd.read_excel("PDAC_Ctrl_All_Cases_Split-30-Test_n=41.xlsx")
 
     # Preprocess training data
-    expression_data = full_data.drop(['ID', 'status'], axis=1)
+    expression_data = train_data.drop(['ID', 'status'], axis=1)
     expression_data_plus1 = np.log2(expression_data + 1)
     expression_data_plus1.columns = expression_data_plus1.columns.astype(str)
     scaler = StandardScaler()
     normalized_data = pd.DataFrame(scaler.fit_transform(expression_data_plus1), columns=expression_data_plus1.columns)
-    normalized_data['ID'] = full_data['ID']
-    normalized_data['status'] = full_data['status']
+    normalized_data['ID'] = train_data['ID']
+    normalized_data['status'] = train_data['status']
 
     # Preprocess test data
     test_expression_data = test_data.drop(['ID', 'status'], axis=1)
@@ -166,14 +166,14 @@ def run_analysis(max_evals, n_startup_jobs, gene_list, csf, csf_name):
     best_model.fit(normalized_data[gene_list].values, normalized_data['status'])
 
     # Prediction and evaluation on the training set
-    X_full = normalized_data[gene_list].values
-    y_full = normalized_data['status']
-    predicted_status_full = best_model.predict(X_full) 
-    predicted_probs_full = best_model.predict_proba(X_full)[:, 1] 
-    normalized_data['Probability'] = predicted_probs_full
+    X_train = normalized_data[gene_list].values
+    y_train = normalized_data['status']
+    predicted_status_train = best_model.predict(X_train) 
+    predicted_probs_train = best_model.predict_proba(X_train)[:, 1] 
+    normalized_data['Probability'] = predicted_probs_train
 
     # Generate confusion matrix and other metrics on the training set
-    conf_matrix = confusion_matrix(y_full, predicted_status_full)
+    conf_matrix = confusion_matrix(y_train, predicted_status_train)
     TN, FP, FN, TP = conf_matrix.ravel()
     sensitivity = TP / (TP + FN) if (TP + FN) != 0 else 0
     specificity = TN / (TN + FP) if (TN + FP) != 0 else 0
@@ -181,42 +181,42 @@ def run_analysis(max_evals, n_startup_jobs, gene_list, csf, csf_name):
     precision = TP / (TP + FP) if (TP + FP) != 0 else 0
     f1_score = 2 * (precision * sensitivity) / (precision + sensitivity) if (precision + sensitivity) != 0 else 0
     f2_score = (5 * precision * sensitivity) / (4 * precision + sensitivity) if (precision + sensitivity) != 0 else 0
-    roc_auc = roc_auc_score(y_full, predicted_probs_full)
-    precision_full, recall_full, _ = precision_recall_curve(y_full, predicted_probs_full)
-    auprc_full = auc(recall_full, precision_full)
-    kappa_train = cohen_kappa_score(y_full, predicted_status_full)
-    mcc = matthews_corrcoef(y_full, predicted_status_full)
-    brier_train = brier_score_loss(y_full, predicted_probs_full)
-    logloss_train = log_loss(y_full, predicted_probs_full)
-    sharpness_train = np.std(predicted_probs_full)
+    roc_auc = roc_auc_score(y_train, predicted_probs_train)
+    precision_train, recall_train, _ = precision_recall_curve(y_train, predicted_probs_train)
+    auprc_train = auc(recall_train, precision_train)
+    kappa_train = cohen_kappa_score(y_train, predicted_status_train)
+    mcc = matthews_corrcoef(y_train, predicted_status_train)
+    brier_train = brier_score_loss(y_train, predicted_probs_train)
+    logloss_train = log_loss(y_train, predicted_probs_train)
+    sharpness_train = np.std(predicted_probs_train)
 
     # Prediction and evaluation on the test set
-    X_test_full = normalized_test_data[gene_list].values
-    y_test_full = normalized_test_data['status']
-    test_set_predictions = best_model.predict(X_test_full) 
-    test_set_pred_probs = best_model.predict_proba(X_test_full)[:, 1]
+    X_test_train = normalized_test_data[gene_list].values
+    y_test_train = normalized_test_data['status']
+    test_set_predictions = best_model.predict(X_test_train) 
+    test_set_pred_probs = best_model.predict_proba(X_test_train)[:, 1]
     normalized_test_data['Probability'] = test_set_pred_probs
 
     # Generate confusion matrix and other metrics on the test set
-    conf_matrix_test = confusion_matrix(y_test_full, test_set_predictions)
+    conf_matrix_test = confusion_matrix(y_test_train, test_set_predictions)
     TN_test, FP_test, FN_test, TP_test = conf_matrix_test.ravel()
     test_sensitivity = TP_test / (TP_test + FN_test) if (TP_test + FN_test) != 0 else 0
     test_specificity = TN_test / (TN_test + FP_test) if (TN_test + FP_test) != 0 else 0
     test_precision = TP_test / (TP_test + FP_test) if (TP_test + FP_test) != 0 else 0
     test_f1_score = 2 * (test_precision * test_sensitivity) / (test_precision + test_sensitivity) if (test_precision + test_sensitivity) != 0 else 0
     test_f2_score = (5 * test_precision * test_sensitivity) / (4 * test_precision + test_sensitivity) if (test_precision + test_sensitivity) != 0 else 0
-    test_mcc = matthews_corrcoef(y_test_full, test_set_predictions)
-    kappa_test = cohen_kappa_score(y_test_full, test_set_predictions)
-    precision_test, recall_test, _ = precision_recall_curve(y_test_full, test_set_pred_probs)
+    test_mcc = matthews_corrcoef(y_test_train, test_set_predictions)
+    kappa_test = cohen_kappa_score(y_test_train, test_set_predictions)
+    precision_test, recall_test, _ = precision_recall_curve(y_test_train, test_set_pred_probs)
     test_auprc = auc(recall_test, precision_test)
-    test_set_accuracy = accuracy_score(y_test_full, test_set_predictions)
-    test_set_auc = roc_auc_score(y_test_full, test_set_pred_probs)
-    brier_test = brier_score_loss(y_test_full, test_set_pred_probs)
-    logloss_test = log_loss(y_test_full, test_set_pred_probs)
+    test_set_accuracy = accuracy_score(y_test_train, test_set_predictions)
+    test_set_auc = roc_auc_score(y_test_train, test_set_pred_probs)
+    brier_test = brier_score_loss(y_test_train, test_set_pred_probs)
+    logloss_test = log_loss(y_test_train, test_set_pred_probs)
     sharpness_test = np.std(test_set_pred_probs)
 
     # Append results for the combination of genes to the dataframe
-    gene_metrics = pd.DataFrame({ 'Gene Combination': ' + '.join(gene_list), 'Best Hyperparameters': [best_trial_params_converted], 'Best Custom Score (CSF)': best_trial_score,'Iteration': best_trial_iteration, 'Training Sensitivity': sensitivity, 'Training Specificity': specificity, 'Training Accuracy': accuracy, 'Training Precision': precision, 'Training F1 Score': f1_score, 'Training F2 Score': f2_score, 'Training MCC': mcc, 'ROC AUC': roc_auc, 'Training AUPRC': auprc_full, 'Training Brier Score': brier_train, 'Training Log Loss': logloss_train, 'Training Sharpness': sharpness_train, 'Training Kappa': kappa_train, 'Training TN': TN,'Training FP': FP, 'Training FN': FN, 'Training TP': TP, 'Test Set Sensitivity': test_sensitivity, 'Test Set Specificity': test_specificity, 'Test Set Accuracy': test_set_accuracy, 'Test Set Precision': test_precision, 'Test Set F1 Score': test_f1_score, 'Test Set F2 Score': test_f2_score, 'Test Set MCC': test_mcc, 'Test Set ROC AUC': test_set_auc, 'Test Set AUPRC': test_auprc, 'Test Brier Score': brier_test, 'Test Log Loss': logloss_test, 'Test Sharpness': sharpness_test, 'Test Kappa': kappa_test, 'Test Set TN': TN_test, 'Test Set FP': FP_test, 'Test Set FN': FN_test, 'Test Set TP': TP_test}, index=[0])
+    gene_metrics = pd.DataFrame({ 'Gene Combination': ' + '.join(gene_list), 'Best Hyperparameters': [best_trial_params_converted], 'Best Custom Score (CSF)': best_trial_score,'Iteration': best_trial_iteration, 'Training Sensitivity': sensitivity, 'Training Specificity': specificity, 'Training Accuracy': accuracy, 'Training Precision': precision, 'Training F1 Score': f1_score, 'Training F2 Score': f2_score, 'Training MCC': mcc, 'ROC AUC': roc_auc, 'Training AUPRC': auprc_train, 'Training Brier Score': brier_train, 'Training Log Loss': logloss_train, 'Training Sharpness': sharpness_train, 'Training Kappa': kappa_train, 'Training TN': TN,'Training FP': FP, 'Training FN': FN, 'Training TP': TP, 'Test Set Sensitivity': test_sensitivity, 'Test Set Specificity': test_specificity, 'Test Set Accuracy': test_set_accuracy, 'Test Set Precision': test_precision, 'Test Set F1 Score': test_f1_score, 'Test Set F2 Score': test_f2_score, 'Test Set MCC': test_mcc, 'Test Set ROC AUC': test_set_auc, 'Test Set AUPRC': test_auprc, 'Test Brier Score': brier_test, 'Test Log Loss': logloss_test, 'Test Sharpness': sharpness_test, 'Test Kappa': kappa_test, 'Test Set TN': TN_test, 'Test Set FP': FP_test, 'Test Set FN': FN_test, 'Test Set TP': TP_test}, index=[0])
 
     all_metrics_df = pd.concat([all_metrics_df, gene_metrics], ignore_index=True)
 
@@ -232,14 +232,14 @@ def run_analysis(max_evals, n_startup_jobs, gene_list, csf, csf_name):
     # Calibrate the model using the calibration set
     calibrated_model = CalibratedClassifierCV(best_model, method='sigmoid', cv='prefit')
     calibrated_model.fit(normalized_calibration_data[gene_list].values, normalized_calibration_data['status'])
-    calibrated_probs_train = calibrated_model.predict_proba(X_full)[:, 1]
-    calibrated_probs_test = calibrated_model.predict_proba(X_test_full)[:, 1]
+    calibrated_probs_train = calibrated_model.predict_proba(X_train)[:, 1]
+    calibrated_probs_test = calibrated_model.predict_proba(X_test_train)[:, 1]
     normalized_data['Calibrated Probability'] = calibrated_probs_train
     normalized_test_data['Calibrated Probability'] = calibrated_probs_test
 
     # Generate confusion matrix and other metrics on the training set with calibrated probabilities
-    calib_predicted_status_full = (calibrated_probs_train >= 0.5).astype(int)
-    conf_matrix_calib = confusion_matrix(y_full, calib_predicted_status_full)
+    calib_predicted_status_train = (calibrated_probs_train >= 0.5).astype(int)
+    conf_matrix_calib = confusion_matrix(y_train, calib_predicted_status_train)
     TN_calib, FP_calib, FN_calib, TP_calib = conf_matrix_calib.ravel()
     calib_sensitivity = TP_calib / (TP_calib + FN_calib) if (TP_calib + FN_calib) != 0 else 0
     calib_specificity = TN_calib / (TN_calib + FP_calib) if (TN_calib + FP_calib) != 0 else 0
@@ -247,38 +247,38 @@ def run_analysis(max_evals, n_startup_jobs, gene_list, csf, csf_name):
     calib_precision = TP_calib / (TP_calib + FP_calib) if (TP_calib + FP_calib) != 0 else 0
     calib_f1_score = 2 * (calib_precision * calib_sensitivity) / (calib_precision + calib_sensitivity) if (calib_precision + calib_sensitivity) != 0 else 0
     calib_f2_score = (5 * calib_precision * calib_sensitivity) / (4 * calib_precision + calib_sensitivity) if (calib_precision + calib_sensitivity) != 0 else 0
-    calib_mcc = matthews_corrcoef(y_full, calib_predicted_status_full)
-    calib_precision_full, calib_recall_full, _ = precision_recall_curve(y_full, calibrated_probs_train)
-    calib_auprc_train = auc(calib_recall_full, calib_precision_full)
-    brier_calib_train = brier_score_loss(y_full, calibrated_probs_train)
-    logloss_calib_train = log_loss(y_full, calibrated_probs_train)
+    calib_mcc = matthews_corrcoef(y_train, calib_predicted_status_train)
+    calib_precision_train, calib_recall_train, _ = precision_recall_curve(y_train, calibrated_probs_train)
+    calib_auprc_train = auc(calib_recall_train, calib_precision_train)
+    brier_calib_train = brier_score_loss(y_train, calibrated_probs_train)
+    logloss_calib_train = log_loss(y_train, calibrated_probs_train)
     sharpness_calib_train = np.std(calibrated_probs_train)
-    kappa_calib_train = cohen_kappa_score(y_full, calib_predicted_status_full)
+    kappa_calib_train = cohen_kappa_score(y_train, calib_predicted_status_train)
 
     # Generate confusion matrix and other metrics on the test set with calibrated probabilities
     calib_test_set_predictions = (calibrated_probs_test >= 0.5).astype(int)
-    conf_matrix_test_calib = confusion_matrix(y_test_full, calib_test_set_predictions)
+    conf_matrix_test_calib = confusion_matrix(y_test_train, calib_test_set_predictions)
     TN_test_calib, FP_test_calib, FN_test_calib, TP_test_calib = conf_matrix_test_calib.ravel()
     test_calib_sensitivity = TP_test_calib / (TP_test_calib + FN_test_calib) if (TP_test_calib + FN_test_calib) != 0 else 0
     test_calib_specificity = TN_test_calib / (TN_test_calib + FP_test_calib) if (TN_test_calib + FP_test_calib) != 0 else 0
     test_calib_precision = TP_test_calib / (TP_test_calib + FP_test_calib) if (TP_test_calib + FP_test_calib) != 0 else 0
     test_calib_f1_score = 2 * (test_calib_precision * test_calib_sensitivity) / (test_calib_precision + test_calib_sensitivity) if (test_calib_precision + test_calib_sensitivity) != 0 else 0
     test_calib_f2_score = (5 * test_calib_precision * test_calib_sensitivity) / (4 * test_calib_precision + test_calib_sensitivity) if (test_calib_precision + test_calib_sensitivity) != 0 else 0
-    test_calib_mcc = matthews_corrcoef(y_test_full, calib_test_set_predictions)
-    calib_precision_test, calib_recall_test, _ = precision_recall_curve(y_test_full, calibrated_probs_test)
+    test_calib_mcc = matthews_corrcoef(y_test_train, calib_test_set_predictions)
+    calib_precision_test, calib_recall_test, _ = precision_recall_curve(y_test_train, calibrated_probs_test)
     test_calib_auprc = auc(calib_recall_test, calib_precision_test)
-    brier_calib_test = brier_score_loss(y_test_full, calibrated_probs_test)
-    logloss_calib_test = log_loss(y_test_full, calibrated_probs_test)
+    brier_calib_test = brier_score_loss(y_test_train, calibrated_probs_test)
+    logloss_calib_test = log_loss(y_test_train, calibrated_probs_test)
     sharpness_calib_test = np.std(calibrated_probs_test)
-    kappa_calib_test = cohen_kappa_score(y_test_full, calib_test_set_predictions)
+    kappa_calib_test = cohen_kappa_score(y_test_train, calib_test_set_predictions)
 
     # Calibrated accuracy and ROC AUC for the training set
-    calib_train_accuracy = accuracy_score(y_full, calib_predicted_status_full)
-    calib_train_auc = roc_auc_score(y_full, calibrated_probs_train)
+    calib_train_accuracy = accuracy_score(y_train, calib_predicted_status_train)
+    calib_train_auc = roc_auc_score(y_train, calibrated_probs_train)
 
     # Calibrated accuracy and ROC AUC for the test set
-    calib_test_accuracy = accuracy_score(y_test_full, calib_test_set_predictions)
-    calib_test_auc = roc_auc_score(y_test_full, calibrated_probs_test)
+    calib_test_accuracy = accuracy_score(y_test_train, calib_test_set_predictions)
+    calib_test_auc = roc_auc_score(y_test_train, calibrated_probs_test)
 
     # Append recalculated performance metrics to gene_metrics
     calib_metrics = pd.DataFrame({'Calib Training Sensitivity': calib_sensitivity, 'Calib Training Specificity': calib_specificity, 'Calib Training Accuracy': calib_train_accuracy, 'Calib Training Precision': calib_precision, 'Calib Training F1 Score': calib_f1_score, 'Calib Training F2 Score': calib_f2_score, 'Calib Training MCC': calib_mcc, 'Calib Training ROC AUC': calib_train_auc, 'Calib Training AUPRC': calib_auprc_train, 'Calib Training Brier Score': brier_calib_train, 'Calib Training Log Loss': logloss_calib_train, 'Calib Training Sharpness': sharpness_calib_train, 'Calib Training Kappa': kappa_calib_train, 'Calib Training TN': TN_calib, 'Calib Training FP': FP_calib, 'Calib Training FN': FN_calib, 'Calib Training TP': TP_calib, 'Calib Test Set Sensitivity': test_calib_sensitivity, 'Calib Test Set Specificity': test_calib_specificity, 'Calib Test Set Accuracy': calib_test_accuracy, 'Calib Test Set Precision': test_calib_precision, 'Calib Test Set F1 Score': test_calib_f1_score, 'Calib Test Set F2 Score': test_calib_f2_score, 'Calib Test Set MCC': test_calib_mcc, 'Calib Test Set ROC AUC': calib_test_auc, 'Calib Test Set AUPRC': test_calib_auprc, 'Calib Test Set Brier Score': brier_calib_test, 'Calib Test Set Log Loss': logloss_calib_test, 'Calib Test Set Sharpness': sharpness_calib_test, 'Calib Test Set Kappa': kappa_calib_test, 'Calib Test Set TN': TN_test_calib, 'Calib Test Set FP': FP_test_calib, 'Calib Test Set FN': FN_test_calib, 'Calib Test Set TP': TP_test_calib}, index=[0])
